610f8667df3a38d63f17a87fdda47efc484f075f
Biagio Ricci <biagior00@gmail.com>
2023-02-01 10:42:27 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Biagio Ricci on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
